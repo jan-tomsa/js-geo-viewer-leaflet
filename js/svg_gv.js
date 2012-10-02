0895d3dfe7904d21dd41b522d8060b4c6f322578
jan-tomsa <jan.tomsa.1976@gmail.com>
2012-10-02 22:23:50 +0200

    added support for SDO rectangles

--- js/svg_gv.js
+++ js/svg_gv.js
@@ -29,10 +29,7 @@ function drawInitial(svg) {
 var colours = ['purple', 'red', 'orange', 'yellow', 'lime', 'green', 'blue', 'navy', 'black'];
 
 function transformHoriz( x ) {
-	gxl = 1 * $("#y1")[0].value;
-	gxr = 1 * $("#y2")[0].value;
-	dx = gxr - gxl;
-	if (Math.abs(x) < 420000) {
+	if (Math.abs(x) < 430000) {
 		return x;  // no transformation
 	} else {
 		rawsx = ((Math.abs(x)-Math.abs(gxr))/dx*1000)
@@ -41,10 +38,7 @@ function transformHoriz( x ) {
 }
 
 function transformVert( y ) {
-	gyb = 1 * $("#x1")[0].value;
-	gyt = 1 * $("#x2")[0].value;
-	dy = gyt - gyb;
-	if (Math.abs(y) < 950000) {
+	if (Math.abs(y) < 910000) {
 		return y;  // no transformation
 	} else {
 		rawsy = (Math.abs(y)-Math.abs(gyt))/dy*700;
@@ -63,7 +57,7 @@ function extractHoriz( coo ) {
 
 function extractVert( coo ) {
 	cooVert = Math.max(Math.abs(coo[0]),Math.abs(coo[1])); 
-	if (cooVert > 900000000) {
+	if (cooVert > 910000000) {
 		return cooVert/1000;
 	} else {
 		return cooVert;
@@ -79,11 +73,27 @@ function gatherWorldCoordinatesFromScript( script ) {
 	return worldCoordinates;
 }
 
-var sdoLinePatt="MDSYS.SDO_GEOMETRY(2002,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(";
-var sdoRectPatt="MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3),MDSYS.SDO_ORDINATE_ARRAY(";
-
 function isNumber( ch ) {
 	return ch.search(/\d/) >= 0;
+}
+
+function processRectangleLine(currentLine, coordinates) {
+	coords = line.split(/[ ,\)]+/);
+	c1 = [ 1*coords[0], 1*coords[1] ];
+	c2 = [ 1*coords[2], 1*coords[3] ];
+	sx1 = transformHoriz(extractHoriz(c1));
+	sy1 = transformVert(extractVert(c1));
+	sx2 = transformHoriz(extractHoriz(c2));
+	sy2 = transformVert(extractVert(c2));
+	x1 = Math.min(sx1,sx2);
+	y1 = Math.min(sy1,sy2);
+	x2 = Math.max(sx1,sx2);
+	y2 = Math.max(sy1,sy2);
+	swidth=x2-x1;
+	sheight=y2-y1;
+	coordinates.push({x1: x1, y1: y1, width: swidth, height: sheight, color: currentColor, strokeWidth: currentWidth, type: 'rect'});
+	lastsx = sx2;
+	lastsy = sy2;
 }
 
 function processCoordinatesLine(currentLine, coordinates) {
@@ -95,14 +105,16 @@ function processCoordinatesLine(currentLine, coordinates) {
 		sy1 = transformVert(extractVert(c1));
 		sx2 = transformHoriz(extractHoriz(c2));
 		sy2 = transformVert(extractVert(c2));
-		coordinates.push({x1: sx1, y1: sy1, x2: sx2, y2: sy2, color: currentColor, width: currentWidth});
+		coordinates.push({x1: sx1, y1: sy1, x2: sx2, y2: sy2, color: currentColor, strokeWidth: currentWidth, type: 'line'});
 		lastsx = sx2;
 		lastsy = sy2;
 	} else if (coords.length == 2) {
 		c2 = [ 1*coords[0], 1*coords[1] ];
 		sx2 = transformHoriz(extractHoriz(c2));
 		sy2 = transformVert(extractVert(c2));
-		coordinates.push({x1: lastsx, y1: lastsy, x2: sx2, y2: sy2, color: currentColor, width: currentWidth});
+		try {
+			coordinates.push({x1: lastsx, y1: lastsy, x2: sx2, y2: sy2, color: currentColor, strokeWidth: currentWidth, type: 'line'});
+		} catch(err) {} // Pokémon
 		lastsx = sx2;
 		lastsy = sy2;
 	}
@@ -112,7 +124,7 @@ function processCoordinatesLine(currentLine, coordinates) {
 				c2 = [ 1*coords[coord], 1*coords[1*coord+1] ];
 				sx2 = transformHoriz(extractHoriz(c2));
 				sy2 = transformVert(extractVert(c2));
-				coordinates.push({x1: lastsx, y1: lastsy, x2: sx2, y2: sy2, color: currentColor, width: currentWidth});
+				coordinates.push({x1: lastsx, y1: lastsy, x2: sx2, y2: sy2, color: currentColor, strokeWidth: currentWidth, type: 'line'});
 				lastsx = sx2;
 				lastsy = sy2;
 			}
@@ -139,20 +151,31 @@ function processCommandLine(currentLine, coordinates) {
 		textValue = args[0];
 		textX = transformHoriz(extractHoriz([args[1],args[2]]));
 		textY = transformVert(extractVert([args[1],args[2]]));
-		coordinates.push({x1: textX, y1: textY, text: textValue, color: currentColor, width: currentWidth});
+		coordinates.push({x1: textX, y1: textY, text: textValue, color: currentColor, strokeWidth: currentWidth, type: 'text'});
 	}
 }
 
+var sdoLinePatt="MDSYS.SDO_GEOMETRY(2002,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(";
+var sdoRectPatt="MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3),MDSYS.SDO_ORDINATE_ARRAY(";
+
 function processScriptLine(currentLine, coordinates) {
 	if (currentLine != "") {
+		geomType = "line";  // default
 		if (currentLine.substr(0,92) == sdoLinePatt) {
 			line = currentLine.substr(92);
+		} else if (currentLine.substr(0,95) == sdoRectPatt) {
+			line = currentLine.substr(95);
+			geomType = "rect";
 		} else {
 			line = currentLine;
 		}
 		firstChar = line.charAt(0);
 		if (isNumber(firstChar)) {
-			processCoordinatesLine(line, coordinates);
+			if (geomType == "line") {
+				processCoordinatesLine(line, coordinates);
+			} else {
+				processRectangleLine(line, coordinates);
+			}
 		} else {
 			processCommandLine(line, coordinates);
 		}
@@ -163,6 +186,12 @@ function processScript() {
 	var myScript = $('#myScript')[0].value;
 	var scriptLines = myScript.split("\n");
 	var coordinates = [];
+	gxl = 1 * $("#y1")[0].value;
+	gxr = 1 * $("#y2")[0].value;
+	dx = gxr - gxl;
+	gyb = 1 * $("#x1")[0].value;
+	gyt = 1 * $("#x2")[0].value;
+	dy = gyt - gyb;
 	lastx = 0;
 	lasty = 0;
 	currentColor = "red"
@@ -178,10 +207,12 @@ function drawSVGLines( coordinates ) {
 	var svg = $('#svgbasics').svg('get');
 	for (lineCoords in coordinates) {
 		lc = coordinates[lineCoords];
-		if (lc.text == null) {
-			svg.line(lc.x1, lc.y1, lc.x2, lc.y2, {stroke: lc.color, 'stroke-width': lc.width});
+		if (lc.type == 'line') {
+			svg.line(lc.x1, lc.y1, lc.x2, lc.y2, {stroke: lc.color, 'stroke-width': lc.strokeWidth});
+		} else if (lc.type == 'rect') {
+			svg.rect(lc.x1, lc.y1, lc.width, lc.height, {fill: 'none',stroke: lc.color, 'stroke-width': lc.strokeWidth});
 		} else {
-			svg.text(null, lc.x1, lc.y1, lc.text, {stroke: lc.color, 'stroke-width': lc.width});
+			svg.text(null, lc.x1, lc.y1, lc.text, {stroke: lc.color, 'stroke-width': lc.strokeWidth});
 		}
 	}
 }
